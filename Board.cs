1c59294fa068f50aebd58603a3bf37eefb1a69b7
PeterMontez <peterson.montez9@gmail.com>
2024-08-20 16:30:11 -0300

    line algorithm under testing

---
+++ Board.cs
@@ -0,0 +1,11 @@
+using System;
+
+// public class Board
+// {
+//     public double[] nails { get; set; }
+
+//     public Board(int diameter, int)
+//     {
+//         this.nails = ;
+//     }
+// }
